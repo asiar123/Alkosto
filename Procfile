ba5a359134ac7dd26d3a0be7d902acd8dca82fb3
asiar_123 <michael_2504@hotmail.com>
2023-05-23 16:23:42 -0500

    Array 3

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn server.wsgi --log-file -
+web: python manage.py migrate && gunicorn server.wsgi --log-file -
--- server/settings.py
+++ server/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'uroot',
+        'PASSWORD': 'pFaEfpnJmR78D5Qkfvyhh',
+        'HOST': 'hcontainers-us-west-157.railway.app',
+        'PORT': '5752',
     }
 }
 
